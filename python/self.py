f46546645cf504383734306054aaf48aa471fdce
thatchanamoorthy2209@gmail.com <thatchanamoorthy2209@gmail.com>
2024-12-28 17:09:33 +0530

    upload

--- python/self.py
+++
@@ -1,9 +0,0 @@
-fname=input("Enter the fisrt name : ")
-lname=input("Enter the last name : ")
-age=input("Enter the age : ")
-fnumber=input("Enter the moblie number : ")
-snumber=input("Enter the second moblie number : ")
-address=input("Enter the address : ")
-space =" "
-
-print("My name is "+ fname + space + lname + space + "My age is " + age + space + " My first moblie number is " + space + fnumber + "My address is "+ address)
